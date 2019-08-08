5f0fa363f8f3ace74c8712c03226e365fe381580
Gowtham Ram Pagadala <pagadalagowtham@gmail.com>
2019-08-08 11:36:04 -0700

    Initial Build

---
+++ scripts/test_slam.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#Launch turtlebot in MapMyWorld
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find my_robot)/world/MapMyWorld.world" &
+sleep 5
+
+#Launch Gmapping
+xterm -e "roslaunch turtlebot_gazebo gmapping_demo.launch" &
+sleep 5
+
+#Launch rviz
+xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 5
+
+#Launch turtlebot teleop
+xterm -e "roslaunch turtlebot_teleop keyboard_teleop.launch"
+
+
